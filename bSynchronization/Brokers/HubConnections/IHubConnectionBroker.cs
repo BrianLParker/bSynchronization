eae26444ac3d4314852edd4d2fc209ab20a3d4d0
Brian Parker <brian.parker@hotmail.com.au>
2021-10-19 00:55:20 +1100

    ShouldStartHubConnection -> FAIL

---
+++ bSynchronization/Brokers/HubConnections/IHubConnectionBroker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace bSynchronization.Brokers.HubConnections
+{
+    public interface IHubConnectionBroker
+    {
+        ValueTask StartAsync();
+        HubConnectionState State { get; }
+        Action<string> OnReceiveMessage { get; }
+        Action<HubConnectionState> OnStatusChanged { get; }
+        ValueTask SendAsync(string groupName, string message);
+        ValueTask JoinGroupAsync(string groupName);
+        ValueTask LeaveGroupAsync(string groupName);
+        ValueTask DisposeAsync();
+    }
+}
---
+++ bSynchronization/Services/SynchronizationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using bSynchronization.Brokers.HubConnections;
+
+namespace bSynchronization.Services
+{
+    public class SynchronizationService : ISynchronizationService
+    {
+        private readonly IHubConnectionBroker hubConnectionBroker;
+
+        public SynchronizationService(IHubConnectionBroker hubConnectionBroker)
+        {
+            this.hubConnectionBroker = hubConnectionBroker;
+        }
+
+        public ValueTask StartAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
